40b5e5450f20756878858ed986d2f5f4ca5bf1e5
LeYunone <365627310@qq.com>
2026-05-07 10:24:56 +0800

    [ADD]导航重置

--- docs/.vuepress/sidebar/talkabout.ts
+++ docs/.vuepress/sidebar/talkabout.ts
@@ -1,23 +1,31 @@
 export const talkabout = [
     {
-        text: "杂谈日记",
+        text: "时评",
+        icon: "android",
+        prefix: "rambling-diary/",
+        collapsible: true,
+        children: [
+            "ai-acgn",
+            "ai-management-vs-developer",
+            "ai-year-2025",
+            "openclaw-hype",
+            "deppseek-recrd",
+            "programmer-core-ability",
+        ]
+    },
+    {
+        text: "生活碎碎念",
         icon: "bigaoshou",
         prefix: "rambling-diary/",
         collapsible: true,
         children: [
             "sms-bombing",
-            "ai-acgn",
-            "ai-management-vs-developer",
-            "ai-year-2025",
-            "deppseek-recrd",
             "info-anxiety",
             "alibaba-error",
             "what-happend-halfyear",
             "FUCKN3ANDrestructure",
-            "openclaw-hype",
             "What-Happened-MYWORK",
             "cyber-manhunt",
-            "programmer-core-ability",
             "qq-secure",
         ]
     },
@@ -32,11 +40,5 @@ export const talkabout = [
             "1.0.2",
             "1.0.3"
         ]
-    },
-    {
-        text: "杂谈",
-        icon: "chat",
-        collapsable: false,
-        children: [],
     },
 ];
